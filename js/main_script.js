870cb23d334536c5cf8b5d95a828bec704e1f0e5
ns2-AlexT <P$20!20$eTsT%!557>
2022-04-12 14:12:53 +0700

    Lesson_2

--- js/main_script.js
+++ js/main_script.js
@@ -1,24 +1,77 @@
-const products = [
-    {id: 1, title: 'Notebook', price: 2000, img_sourse: "img/good_3.svg", img_gefault: "img/good_default.png"},
-    {id: 2, title: 'Mouse', price: 20, img_sourse: "img/good_2.svg", img_gefault: "img/good_default.png"},
-    {id: 3, title: 'Keyboard', price: 200, img_sourse: "img/good_3.png", img_gefault: "img/good_default.png"},
-    {id: 4, title: 'Gamepad', price: 50, img_sourse: "img/good_4.svg", img_gefault: "img/good_default.png"},
-];
+class CatalogOfGood {
+    constructor(container = '.container_for_goods') {
+        this.container = document.querySelector(container);
+        this.list_of_goods = [];
+        this._addGoods();
+    }
 
-const renderProduct = (item) => {
+    _addGoods() {
+        this.list_of_goods = [
+            {id: 1, title: 'Notebook', price: 2000, img_sourse: "img/good_3.svg"},
+            {id: 2, title: 'Mouse', price: 20, img_sourse: "img/good_2.svg"},
+            {id: 3, title: 'Keyboard', price: 200, img_sourse: "img/good_3.png"},
+            {id: 4, title: 'Gamepad', price: 50, img_sourse: "img/good_4.svg"},
+        ];
+    }
 
-      return `<div class="block_of_good">
-                <img class="good_img_" src=${item.img_sourse} onerror="src='${item.img_gefault}'">
+    render() {
+        for (let good of this.list_of_goods) {
+            const makeGoogObject = new GoodItem(good);
+            this.container.insertAdjacentHTML('beforeend', makeGoogObject.makeHTML());
+        }
+    }
+
+    countTotalPrice() {
+        let sum = 0;
+        this.list_of_goods.forEach(function (item) {sum += item.price});
+        return sum;
+    }
+}
+
+class GoodItem {
+    constructor(good) {
+        this.id = good.id;
+        this.title = good.title;
+        this.price = good.price;
+        this.img = good.img_sourse;
+    }
+
+    makeHTML() {
+        return `<div class="block_of_good">
+                <img class="good_img_" src=${this.img} onError="this.img='good_default.png'">
+<!--                onerror="src=this.img_default"-->
                 <div class="card-body">
-                    <h5 class="card-title">${item.title}</h5>
-                    <p>price: ${item.price}</p>
+                    <h5 class="card-title">${this.title}</h5>
+                    <p>price: ${this.price}</p>
                     <a href="#" class="top_buttion">Buy now</a>
                 </div>
             </div>`
-};
-const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item));
-    document.querySelector('.container_for_goods').innerHTML = productsList.join("");
-};
+    }
+}
 
-renderPage(products);
+// Class of good in basket
+class GoodItemBasket extends GoodItem{
+    constructor(good, quantity){
+        super(good);
+        this.quantity = quantity;
+    }
+}
+// // Class of basket
+class Basket{
+    constructor(container = '.basket'){
+        this.container = container;
+        this.basket_items = [];
+    }
+    _delItem(){
+    }
+    _addItem(){
+    }
+    _countSumOfItem(){
+    }
+    totalPriceOfBasket(){
+    }
+}
+
+let make_list = new CatalogOfGood()
+make_list.render()
+console.log('Total price is ', make_list.countTotalPrice())
